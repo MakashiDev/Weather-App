cdb7e592856868105fc32e14842abc79585178de
Makashi <53072442+Makashi2021@users.noreply.github.com>
2021-11-02 17:00:21 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
+        alert("Geolocation is not supported by this browser.");
     }
 }
 
@@ -13,41 +13,36 @@ function showPosition(position) {
     const lon = position.coords.longitude;
     document.cookie = "lat=" + lat;
     document.cookie = "lon=" + lon;
-    console.log(document.cookie);
-    console.log(lat, lon);
     const API = "942d172e2e4e59b8e961f7dda58888f5";
-    fetch(
-            "https://api.openweathermap.org/data/2.5/weather?lat=" +
-            lat +
-            "&lon=" +
-            lon +
-            "&appid=" +
-            API
-        )
+    fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + API)
         .then((response) => response.json())
         .then((data) => printWeather(data));
 }
 
+function getCity() {
+    const city = document.getElementById("city").value;
+    showCity(city);
+}
+
+function showCity(city) {
+    const API = "942d172e2e4e59b8e961f7dda58888f5";
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + API)
+        .then((response) => response.json())
+        .then((data) => printWeather(data));
+
+}
+
 function printWeather(weather) {
-    console.log(weather);
     temp = weather.main.temp;
     if (weather.sys.country == "US") {
         temp = (temp - 273.15) * 1.8 + 32;
         temp = Math.round(temp);
         humidity = weather.main.humidity;
         wind = weather.wind.speed;
-        console.log(temp);
         document.getElementById("temp").innerHTML = temp + "°F";
-        document.getElementById("humidity").innerHTML = "Humidity:" + humidity;
-        document.getElementById("wind").innerHTML =
-            "Wind: " + Math.round(wind) + "mph";
-    } else {
-        temp = temp - 273.15;
-        console.log(temp);
-        temp = Math.round(temp);
-        document.getElementById("demo").innerHTML = temp;
+        document.getElementById("humidity").innerHTML = "Humidity: " + humidity + "%";
+        document.getElementById("place").innerHTML = "Weather in " + weather.name
+        document.getElementById("wind").innerHTML = "Wind: " + Math.round(wind) + "mph";
     }
 }
-document
-    .getElementsByClassName("center")
-    .addEventListener("load", getLocation());
+document.getElementsByClassName("center").addEventListener("load", getLocation());
